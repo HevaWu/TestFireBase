23fe418153fc06a444261a330be11d48391b1228
Heva <he.wu@linecorp.com>
2019-06-26 17:33:33 +0900

    Add TestFireBase project

---
+++ TestFireBase/TestFireBase/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  TestFireBase
+//
+//  Created by ST21235 on 2019/06/26.
+//  Copyright © 2019 He Wu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
